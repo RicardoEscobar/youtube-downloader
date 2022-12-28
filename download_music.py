a905870224f966eaa311818c4ac63fd966b9d40a
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-12-28 17:24:29 -0600

    Add download_music.py

---
+++ download_music.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import pytube
+from time_it import time_it
+
+
+@time_it
+def download_music(url: str) -> None:
+    # Create a YouTube object using the URL
+    yt = pytube.YouTube(url)
+
+    # Get the highest quality audio stream available
+    audio_stream = yt.streams.filter(only_audio=True).first()
+
+    # Save the audio stream to a file
+    output_file = Path(audio_stream.default_filename).with_suffix('.mp3')
+    print(repr(output_file))
+    audio_stream.download(filename=output_file)
+
+
+if __name__ == '__main__':
+    # Replace this with the URL of the YouTube video you want to download
+    download_music("https://youtu.be/qpCNaRkIh2E")
