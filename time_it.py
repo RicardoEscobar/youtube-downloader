8e7bc577b230ad79cd77cba7c4f33f0b98da39a3
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-12-28 17:18:53 -0600

    Add time_it.py

---
+++ time_it.py
@@ -0,0 +1,11 @@
+import time
+
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"{func.__name__} took {end - start:.2f} seconds")
+        return result
+    return wrapper
