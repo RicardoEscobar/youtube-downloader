9e8ae578396369cc03ec373e53d89562bbeb35d0
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-12-28 18:34:54 -0600

    Remove geometry and no-resize, create frames for controls and progress bar app.py

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from download_music import download_music
 
 # Create the main window
 root = tk.Tk()
 root.title("YouTube Downloader")
-root.resizable(False, False)
-# Set the width to 300 pixels and the height to 50 pixels
-root.geometry("300x50")
+# root.resizable(False, False)
+# Set the width to 300 pixels and the height to 100 pixels
+# root.geometry("300x100")
 
 # Create a frame and pack it into the main window
 frame = tk.Frame(root, padx=10, pady=10)
 frame.pack()
 
+# Create a frame_audio_download and pack it into the frame, with padding
+frame_audio_download = tk.Frame(frame, padx=10, pady=10)
+frame_audio_download.pack()
+
+# Create a frame_progress and pack it into the frame, with padding
+frame_progress = tk.Frame(frame, padx=10, pady=10)
+frame_progress.pack()
+
 # Create a text input and a button, and pack them into the frame
-youtube_url_input = tk.Entry(frame)
+youtube_url_input = tk.Entry(frame_audio_download)
 youtube_url_input.pack(side='left')
-download_button = tk.Button(frame, text='Download', command=lambda: download_music(
+download_button = tk.Button(frame_audio_download, text='Download', command=lambda: download_music(
     youtube_url_input.get()))  # Retrieve the text from the text_input widget)
 download_button.pack(side='left')
+
+# Create a progress bar and pack it into the frame
+progress_bar = ttk.Progressbar(frame_progress, orient='horizontal', length=300)
+progress_bar.pack(side='top')
 
 # Run the Tkinter event loop
 root.mainloop()
