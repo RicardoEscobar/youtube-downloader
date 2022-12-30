7a21b2e2066d4357da25c5e99b185e6743ef32ee
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-12-30 16:16:30 -0600

    Fixes frame to use all space within the main window app.py

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ root.title("YouTube Downloader")
 # root.geometry("300x100")
 
 # Create a frame and pack it into the main window
-frame = tk.Frame(root, padx=10, pady=10)
-frame.pack()
+frame = tk.Frame(root, padx=10, pady=10, bg='red')
+frame.pack(expand=True, fill='both')
 
 # Create a frame_audio_download and pack it into the frame, with padding
 frame_audio_download = tk.Frame(frame, padx=10, pady=10)
-frame_audio_download.pack()
+frame_audio_download.pack(expand=True, fill='x')
 
 # Create a frame_progress and pack it into the frame, with padding
 frame_progress = tk.Frame(frame, padx=10, pady=10)
-frame_progress.pack()
+frame_progress.pack(expand=True, fill='x')
 
 # Create a text input and a button, and pack them into the frame
 youtube_url_input = tk.Entry(frame_audio_download)
@@ -32,7 +32,7 @@ download_button.pack(side='right')
 
 # Create a progress bar and pack it into the frame
 progress_bar = ttk.Progressbar(frame_progress, orient='horizontal', length=300)
-progress_bar.pack(side='top')
+progress_bar.pack(side='left', fill='x', expand=True)
 
 # Run the Tkinter event loop
 root.mainloop()
