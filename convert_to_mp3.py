7655b5949046dcde5888114eebdb3c3bc1f15e99
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-12-31 03:43:06 -0600

    Add webm to mp3 support to convert_to_mp3.py

--- convert_to_mp3.py
+++ convert_to_mp3.py
@@ -5,8 +5,18 @@ from time_it import time_it
 
 @time_it
 def convert_to_mp3(input_file: Path, output_file: Path) -> None:
-    command = f'ffmpeg -i "{input_file}" -vn -acodec libmp3lame -y "{output_file}"'
-    subprocess.run(command, shell=True, check=True)
+    if input_file.suffix == '.webm':
+        output_file_mp4 = input_file.with_suffix(".mp4")
+        command = f'ffmpeg -i "{input_file}" -c:a libmp3lame -y "{output_file_mp4}"'
+        subprocess.run(command, shell=True, check=True)
+        command = f'ffmpeg -i "{output_file_mp4}" -vn -acodec libmp3lame -y "{output_file}"'
+        subprocess.run(command, shell=True, check=True)
+    else:
+        command = f'ffmpeg -i "{input_file}" -vn -acodec libmp3lame -y "{output_file}"'
+        subprocess.run(command, shell=True, check=True)
+
+    # clean up
+    output_file_mp4.unlink()
 
 
 if __name__ == '__main__':
