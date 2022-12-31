a390c322ed44c3959b0fff193f5b8d23201d4d86
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-12-30 18:33:13 -0600

    Refactoring download_audio.py

--- download_audio.py
+++ download_audio.py
@@ -4,7 +4,7 @@ from time_it import time_it
 
 
 @time_it
-def download_music(url: str) -> None:
+def download_audio(url: str) -> None:
     # Create a YouTube object using the URL
     yt = pytube.YouTube(url)
 
@@ -13,10 +13,10 @@ def download_music(url: str) -> None:
 
     # Save the audio stream to a file
     output_file = Path(audio_stream.default_filename).with_suffix('.mp3')
-    print(repr(output_file))
+    print(output_file)
     audio_stream.download(filename=output_file)
 
 
 if __name__ == '__main__':
     # Replace this with the URL of the YouTube video you want to download
-    download_music("https://youtu.be/qpCNaRkIh2E")
+    download_audio("https://youtu.be/qpCNaRkIh2E")
