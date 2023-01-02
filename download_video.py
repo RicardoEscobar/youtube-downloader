b76f5c84426f0736a5365e748561c98981951e78
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-01-02 04:27:45 -0600

    Add progress bar support passing progress_bar, progress_bar_variable. I'll refactor it.

--- download_video.py
+++ download_video.py
@@ -1,12 +1,27 @@
 from pathlib import Path
+import tkinter as tk
+import tkinter.ttk as ttk
 import pytube
 from time_it import time_it
+from update_progress_bar import update_progress_bar
 
 
 @time_it
-def download_video(url: str) -> None:
-    # Create a YouTube object using the URL
-    yt = pytube.YouTube(url)
+def download_video(url: str, progress_bar_variable: tk.IntVar = None, progress_bar: ttk.Progressbar = None) -> None:
+    print(
+        f'download_video:progress_bar_variable -> {progress_bar_variable.get()}')
+    # Create a YouTube object using the URL, add progress_bar_variable if able
+    yt = pytube.YouTube(
+        url,
+        on_progress_callback=lambda stream, chunk, bytes_remaining:
+        # Lambda used to pass the progress_bar_variable argument to update_progress_bar function
+        update_progress_bar(stream=stream,
+                            chunk=chunk,
+                            bytes_remaining=bytes_remaining,
+                            progress_bar_variable=progress_bar_variable,
+                            progress_bar=progress_bar
+                            )
+    )
 
     # Get the list of available streams
     streams = yt.streams.filter(progressive=True)
--- update_progress_bar.py
+++ update_progress_bar.py
@@ -4,7 +4,13 @@ import tkinter.ttk as ttk
 
 
 # Create a function to update the progress bar
-def update_progress_bar(stream: pytube.Stream, chunk, bytes_remaining, progress_bar: ttk.Progressbar):
+def update_progress_bar(stream: pytube.Stream,
+                        chunk,
+                        bytes_remaining,
+                        progress_bar_variable: tk.IntVar = None,
+                        progress_bar: ttk.Progressbar = None):
+    print(
+        f'update_progress_bar:progress_bar_variable -> {progress_bar_variable.get()}')
     # Get total_size from stream in bytes
     total_size = stream.filesize
 
@@ -13,10 +19,10 @@ def update_progress_bar(stream: pytube.Stream, chunk, bytes_remaining, progress_
     print(f'{percentage}%')
 
     # Update the progress bar with the percentage
-    progress_bar["value"] = percentage
+    if progress_bar_variable:
+        progress_bar_variable.set(percentage)
+        print(
+            f'progress_bar_variable.set(percentage) -> {progress_bar_variable.get()}')
 
-    # Update the progress bar with text
-    progress_bar["value"] = percentage
-
-    # Refresh the display of the progress bar
-    progress_bar.update()
+        # Refresh the display of the progress bar
+        progress_bar.update()
