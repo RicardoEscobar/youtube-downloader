0a97be8e52985d14a2840437f88ed641c1d8c4d9
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-01-03 17:38:10 -0600

    Add on_progress_callback, on_complete_callback functions to download_highest_quality_audio.py

--- download_highest_quality_audio.py
+++ download_highest_quality_audio.py
@@ -1,13 +1,32 @@
 from pathlib import Path
+import tkinter as tk
+import tkinter.ttk as ttk
 import pytube
 from time_it import time_it
+from update_progress_bar import update_progress_bar
+from complete_progress_bar import complete_progress_bar
 
 
 @time_it
-def download_highest_quality_audio(url: str) -> Path:
+def download_highest_quality_audio(url: str, progress_bar_variable: tk.IntVar = None, progress_bar: ttk.Progressbar = None) -> Path:
     """Download the highest quality audio of a YouTube video"""
     # Create a YouTube object using the URL
-    yt = pytube.YouTube(url)
+    yt = pytube.YouTube(
+        url=url,
+        on_progress_callback=lambda stream, chunk, bytes_remaining:
+        # Lambda used to pass the progress_bar_variable argument to update_progress_bar function
+        update_progress_bar(stream=stream,
+                            bytes_remaining=bytes_remaining,
+                            progress_bar_variable=progress_bar_variable,
+                            progress_bar=progress_bar
+                            ),
+        on_complete_callback=lambda stream, file_handle:
+        complete_progress_bar(
+            stream=stream,
+            file_handle=file_handle,
+            progress_bar_variable=progress_bar_variable,
+            progress_bar=progress_bar)
+    )
 
     # Get the highest quality audio stream
     audio_stream = yt.streams.get_by_itag(yt.streams.filter(
