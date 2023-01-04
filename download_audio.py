df8e8c8b20bb47e5234415aa1f10cef7e323ebfb
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-01-03 19:27:28 -0600

    Add 'prime audio' to output file in download_audio.py

--- download_audio.py
+++ download_audio.py
@@ -9,7 +9,9 @@ from convert_to_mp3 import convert_to_mp3
 def download_audio(url: str, progress_bar_variable: tk.IntVar = None, progress_bar: ttk.Progressbar = None) -> None:
     audio = download_highest_quality_audio(
         url, progress_bar_variable=progress_bar_variable, progress_bar=progress_bar)
-    output = audio.with_name(audio.stem + ' (prime)' + '.mp3')
+    output = audio.with_name(audio.stem + ' (prime audio)' + '.mp3')
+
+    # Convert downloaded audio file into mp3 file
     convert_to_mp3(audio, output)
 
     # clean up
