267d944a63d58235fe88b1446cebc688b723df5a
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-01-03 18:59:58 -0600

    Refactor update_progress_bar in update_progress_bar.py

--- update_progress_bar.py
+++ update_progress_bar.py
@@ -4,10 +4,11 @@ import tkinter.ttk as ttk
 
 
 # Create a function to update the progress bar
-def update_progress_bar(stream: pytube.Stream,
-                        bytes_remaining,
-                        progress_bar_variable: tk.IntVar = None,
-                        progress_bar: ttk.Progressbar = None):
+def update_progress_bar(
+        stream: pytube.Stream,
+        bytes_remaining,
+        progress_bar_variable: tk.IntVar = None,
+        progress_bar: ttk.Progressbar = None):
     print(
         f'update_progress_bar:progress_bar_variable -> {progress_bar_variable.get()}')
     # Get total_size from stream in bytes
