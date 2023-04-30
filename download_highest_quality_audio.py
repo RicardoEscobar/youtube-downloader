cb5a4b7e22ac4382c99d103b69ccee059165c475
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-04-29 19:40:35 -0600

    Add oauth authentication and  output parameter to: download_highest_quality_audio.py

--- download_highest_quality_audio.py
+++ download_highest_quality_audio.py
@@ -4,18 +4,22 @@ from time_it import time_it
 
 
 @time_it
-def download_highest_quality_audio(url: str) -> Path:
+def download_highest_quality_audio(url: str, output: str = None) -> Path:
     """Download the highest quality audio of a YouTube video"""
     # Create a YouTube object using the URL
-    yt = pytube.YouTube(url)
+    yt = pytube.YouTube(url, use_oauth=True, allow_oauth_cache=True)
 
     # Get the highest quality audio stream
     audio_stream = yt.streams.get_by_itag(yt.streams.filter(
         only_audio=True).order_by('abr').desc().first().itag)
 
     # Save the audio stream to a file
-    output_file = Path(audio_stream.default_filename)
-    print(output_file)
+    if output is None:
+        output_file = Path(audio_stream.default_filename)
+    else:
+        output_file = Path(output)
+
+    # Download the audio stream
     audio_stream.download(filename=output_file)
 
     # Return a path to the output file
